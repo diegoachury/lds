a10be6c7af4375dba76acf35665104265476c981
Diego Achury <achurycompany@gmail.com>
2013-05-22 15:02:08 -0500

    creando repositorio lds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lds::Application.config.secret_token = '784c69029be83716e7655715b9ef5149a8b0db5d0b40b02260597acd94396196eb31a328bf211feae51f7abd65415692e8804876e1941b68ba8e3b7926c9badc'
